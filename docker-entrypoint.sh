4086bf758094d5f51dd213f4b22cb3a6df02efbc
AngeKano <Angekano002@gmail.com>
2025-12-10 09:12:48 +0000

    un update files docker migration #1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
-node ./node_modules/prisma/build/index.js db push --skip-generate --accept-data-loss
-
 echo "Starting application..."
 exec node server.js
