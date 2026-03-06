3d842251d2dd7827692ee207691bc2330a536d46
AngeKano <Angekano002@gmail.com>
2026-03-06 10:21:51 +0000

    Add barCategoryGap to ClientReportingChart for improved spacing

--- src/components/reporting/client-reporting-chart.tsx
+++ src/components/reporting/client-reporting-chart.tsx
@@ -855,6 +855,7 @@ export default function ClientReportingChart({
           <BarChart
             data={visibleChartData}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            barCategoryGap="20%"
           >
             <CartesianGrid strokeDasharray="3 3" vertical={false} />
             <XAxis
